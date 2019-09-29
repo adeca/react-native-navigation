90fbfedbe65739c7c25f19dfcbf8b600cd58e66e
Agustin de Cabrera <agustindc@gmail.com>
2019-09-29 12:20:02 -0300

    Fixed crash in iOS 13

--- lib/ios/RNNNavigationBar.m
+++ lib/ios/RNNNavigationBar.m
@@ -4,9 +4,14 @@
 
 - (void)layoutSubviews {
 	[super layoutSubviews];
+	self.layoutMargins = UIEdgeInsetsZero;
 
 	for (UIView *view in self.subviews) {
-		view.layoutMargins = UIEdgeInsetsZero;
+		UIEdgeInsets margins = view.layoutMargins;
+		CGRect frame = view.frame;
+		frame.origin.x = -margins.left;
+		frame.size.width += margins.left + margins.right;
+		view.frame = frame;
 	}
 }
 
